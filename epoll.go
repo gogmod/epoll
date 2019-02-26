10e395e9b5c5d3051cc32de38f68f2a94e1374bd
xingcuntian <xingcuntian@qq.com>
2019-02-26 16:36:58 +0800

    update

--- epoll.go
+++ epoll.go
@@ -1,87 +1,88 @@
 package epoll
 
 import (
-    "golang.org/x/sys/unix"
-    "github.com/sirupsen/logrus"
-    "net"
-    "reflect"
-    "sync"
-    "syscall"
+	"net"
+	"reflect"
+	"sync"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 )
 
-func SetLimit(cur,max uint64)(err error){
-    err = syscall.Setrlimit(syscall.RLIMIT_NOFILE,&syscall.Rlimit{Cur:cur,Max:max})
-    return
+func SetLimit(cur, max uint64) (err error) {
+	err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &syscall.Rlimit{Cur: cur, Max: max})
+	return
 }
 
-type epoll struct {
-    fd      int
-    connections map[int]net.Conn
-    lock    *sync.RWMutex
+type Epoll struct {
+	fd          int
+	connections map[int]net.Conn
+	lock        *sync.RWMutex
 }
 
-func MkEpoll() (*epoll, error) {
-    fd, err := unix.EpollCreate1(0)
-    if err != nil {
-        return nil, err
-    }
+func MkEpoll() (*Epoll, error) {
+	fd, err := unix.EpollCreate1(0)
+	if err != nil {
+		return nil, err
+	}
 
-    return &epoll{
-        fd:     fd,
-        lock:   &sync.RWMutex{},
-        connections: make(map[int]net.Conn),
-    }, nil
+	return &Epoll{
+		fd:          fd,
+		lock:        &sync.RWMutex{},
+		connections: make(map[int]net.Conn),
+	}, nil
 }
 
-func (e *epoll) Add(conn net.Conn) (err error){
-    fd := socketFD(conn)
-    err = unix.EpollCtl(e.fd, syscall.EPOLL_CTL_ADD, fd , &unix.EpollEvent{Events: unix.POLLIN | unix.POLLHUP, Fd: int32(fd)})
-    if err != nil {
-        return err
-    }
-    e.lock.Lock()
-    defer e.lock.Unlock()
-    e.connections[fd] = conn
-    if len(e.connections)%100 == 0 {
-        logrus.Infof("Total number of connections: %v", len(e.connections))
-    }
-    return nil
+func (e *Epoll) Add(conn net.Conn) (err error) {
+	fd := socketFD(conn)
+	err = unix.EpollCtl(e.fd, syscall.EPOLL_CTL_ADD, fd, &unix.EpollEvent{Events: unix.POLLIN | unix.POLLHUP, Fd: int32(fd)})
+	if err != nil {
+		return err
+	}
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	e.connections[fd] = conn
+	if len(e.connections)%100 == 0 {
+		logrus.Infof("Total number of connections: %v", len(e.connections))
+	}
+	return nil
 }
 
-func (e *epoll) Remove(conn net.Conn) (err error){
-    fd := socketFD(conn)
-    err = unix.EpollCtl(e.fd, syscall.EPOLL_CTL_DEL, fd, nil)
-    if err != nil {
-        return err
-    }
-    e.lock.Lock()
-    defer e.lock.Unlock()
-    delete(e.connections,fd)
-    if len(e.connections)%100 == 0 {
-        logrus.Infof("Total number of connections: %v", len(e.connections))
-    }
-    return nil
+func (e *Epoll) Remove(conn net.Conn) (err error) {
+	fd := socketFD(conn)
+	err = unix.EpollCtl(e.fd, syscall.EPOLL_CTL_DEL, fd, nil)
+	if err != nil {
+		return err
+	}
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	delete(e.connections, fd)
+	if len(e.connections)%100 == 0 {
+		logrus.Infof("Total number of connections: %v", len(e.connections))
+	}
+	return nil
 }
 
-func (e *epoll) Wait() ([]net.Conn, error) {
-    events := make([]unix.EpollEvent, 100)
-    n, err := unix.EpollWait(e.fd, events, 100)
-    if err != nil {
-        return nil, err
-    }
-    e.lock.RLock()
-    defer e.lock.RUnlock()
-    var connections []net.Conn
-    for i := 0; i < n; i++ {
-        conn := e.connections[int(events[i].Fd)]
-        connections = append(connections,conn)
-    }
-    return connections, nil
+func (e *Epoll) Wait() ([]net.Conn, error) {
+	events := make([]unix.EpollEvent, 100)
+	n, err := unix.EpollWait(e.fd, events, 100)
+	if err != nil {
+		return nil, err
+	}
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	var connections []net.Conn
+	for i := 0; i < n; i++ {
+		conn := e.connections[int(events[i].Fd)]
+		connections = append(connections, conn)
+	}
+	return connections, nil
 }
 
 func socketFD(conn net.Conn) int {
-    tcpConn := reflect.Indirect(reflect.ValueOf(conn)).FieldByName("conn")
-    fdVal := tcpConn.FieldByName("fd")
-    pfdVal := reflect.Indirect(fdVal).FieldByName("pfd")
-    return int(pfdVal.FieldByName("Sysfd").Int())
+	tcpConn := reflect.Indirect(reflect.ValueOf(conn)).FieldByName("conn")
+	fdVal := tcpConn.FieldByName("fd")
+	pfdVal := reflect.Indirect(fdVal).FieldByName("pfd")
+	return int(pfdVal.FieldByName("Sysfd").Int())
 }
